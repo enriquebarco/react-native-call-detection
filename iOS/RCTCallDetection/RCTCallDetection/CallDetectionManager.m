93944a64c199d56721ec3e52fb95e5a025a65f43
Enrique <enrique.barco96@hotmail.com>
2024-10-22 16:47:53 -0400

    trying to add background tasks

--- iOS/RCTCallDetection/RCTCallDetection/CallDetectionManager.m
+++ iOS/RCTCallDetection/RCTCallDetection/CallDetectionManager.m
@@ -13,11 +13,27 @@
 typedef void (^CallBack)();
 @interface CallDetectionManager()
 
+
+
+
 @property(strong, nonatomic) RCTResponseSenderBlock block;
+
 @property(strong, nonatomic) CXCallObserver* callObserver;
 
+@property(nonatomic) long* _backgroundUpdateTask;
+
+
+
+
 @end
+
+
+
+
 @implementation CallDetectionManager
+
+
+
 
 - (NSArray<NSString *> *)supportedEvents {
     return @[@"PhoneCallStateUpdate"];
@@ -35,30 +51,96 @@ RCT_EXPORT_METHOD(addCallBlock:(RCTResponseSenderBlock) block) {
     self.callObserver = [[CXCallObserver alloc] init];
     __typeof(self) weakSelf = self;
     [self.callObserver setDelegate:weakSelf queue:nil];
+
 }
 
+
+
+
 RCT_EXPORT_METHOD(startListener) {
+
+    [self beginBackgroundUpdateTask];
+
     // Setup call tracking
+
     self.callObserver = [[CXCallObserver alloc] init];
+
     __typeof(self) weakSelf = self;
     [self.callObserver setDelegate:weakSelf queue:nil];
 }
 
+
 RCT_EXPORT_METHOD(stopListener) {
+
     // Setup call tracking
+
     self.callObserver = nil;
+
+    [self endBackgroundUpdateTask];
+
 }
 
-- (void)callObserver:(CXCallObserver *)callObserver callChanged:(CXCall *)call {
-    if (call.hasEnded == true) {
-      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Disconnected"];
-    } else if (call.hasConnected == true) {
-      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Connected"];
-    } else if (call.isOutgoing == true) {
-      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Dialing"];
-    } else {
-      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Incoming"];
-    }
+
+
+
+- (void) beginBackgroundUpdateTask
+
+{
+
+    self._backgroundUpdateTask = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
+
+        [self endBackgroundUpdateTask];
+
+    }];
+
 }
+
+
+
+
+- (void) endBackgroundUpdateTask
+
+{
+
+    [[UIApplication sharedApplication] endBackgroundTask: self._backgroundUpdateTask];
+
+    self._backgroundUpdateTask = UIBackgroundTaskInvalid;
+
+}
+
+
+
+
+- (void)callObserver:(CXCallObserver *)callObserver callChanged:(CXCall *)call {
+
+    if (call.hasEnded == true) {
+
+      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Disconnected"];
+
+    }
+
+    if (call.isOutgoing == true && call.hasConnected == false && call.hasEnded == false) {
+
+
+      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Dialing"];
+
+    }
+
+    if (call.isOutgoing == false && call.hasConnected == false) {
+
+      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Incoming"];
+
+    }
+
+    if (call.hasEnded == false && call.hasConnected == true) {
+
+      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Connected"];
+
+    }
+
+}
+
+
+
 
 @end
